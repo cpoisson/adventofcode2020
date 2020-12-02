27262ef27912836379e48c15f7c38ad0e23ec0c4
Charles Poisson <charles.poisson@gmail.com>
2020-12-03 00:20:23 +0100

    First commit

---
+++ day_01/solve_puzzles.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""Advent of Code 2020: Day 1"""
+
+import time
+import re
+
+def rule_match_cnt(rule: str, password: str) -> bool:
+    char = str(rule.split(' ')[1])
+    min_cnt = int(rule.split(' ')[0].split('-')[0])
+    max_cnt = int(rule.split(' ')[0].split('-')[1])
+    if (password.count(char) > max_cnt) or (password.count(char) < min_cnt):
+        return False
+    return True
+
+def rule_match_pos(rule: str, password: str) -> bool:
+    char = str(rule.split(' ')[1])
+    pos_1 = int(rule.split(' ')[0].split('-')[0]) - 1
+    pos_2 = int(rule.split(' ')[0].split('-')[1]) - 1
+    if (password[pos_1] == char) ^ (password[pos_2] == char):
+        return True
+    return False
+
+def puzzle_1(passwords):
+    return len([password[1] for password in passwords if rule_match_cnt(password[0], password[1])])
+
+def puzzle_2(passwords):
+    return len([password[1] for password in passwords if rule_match_pos(password[0], password[1])])
+
+
+def stopwatch(func, arg):
+    t_start = time.process_time_ns()
+    result = func(arg)
+    t_elapsed = time.process_time_ns() - t_start
+    return "⚙️ {} -> {} -> ⏱️ {} ms".format(func.__name__, result, t_elapsed/1000)
+
+# Main
+def main():
+    with open("passwords.txt",  "r") as f:
+        passwords = [line.rstrip().split(":") for line in f.readlines()]
+        passwords = [[password[0], password[1].lstrip()] for password in passwords]
+
+    print("Puzzle 1: Password count matching the rule count.")
+    print(stopwatch(puzzle_1, passwords))
+
+    print("Puzzle 2: Password count matching the rule pos.")
+    print(stopwatch(puzzle_2, passwords))
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_02/solve_puzzles.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+"""Advent of Code 2020: Day 1"""
+
+import itertools
+import time
+
+# loops
+
+def puzzle_1_loops(expenses):
+    for i in range(0, len(expenses)):
+        for j in reversed(range(0, len(expenses))):
+            expense_sum = expenses[i] + expenses[j]
+            if expense_sum == 2020:
+                expense_prod = expenses[i] * expenses[j]
+                return (expenses[i], expenses[j]), expense_sum, expense_prod
+    return ()
+
+def puzzle_2_loops(expenses):
+    for i in range(0, len(expenses)):
+        for j in range(0, len(expenses)):
+            if j == i:
+                pass
+            for k in range(0, len(expenses)):
+                if k == i or k == j:
+                    pass
+                expense_sum = expenses[i] + expenses[j] + expenses[k]
+                if expense_sum == 2020:
+                    expense_prod = expenses[i] * expenses[j] * expenses[k]
+                    return (expenses[i], expenses[j], expenses[k]), expense_sum, expense_prod
+    return ()
+
+# permutations
+
+def puzzle_1_permutations(expenses):
+    for expense_tuple in itertools.permutations(expenses, 2):
+        expense_sum = sum(expense_tuple)
+        if expense_sum == 2020:
+            expense_prod = expense_tuple[0] * expense_tuple[1]
+            return expense_tuple, expense_sum, expense_prod
+    return ()
+
+def puzzle_2_permutations(expenses):
+    for expense_tuple in itertools.permutations(expenses, 3):
+        expense_sum = sum(expense_tuple)
+        if expense_sum == 2020:
+            expense_prod = expense_tuple[0] * expense_tuple[1] * expense_tuple[2]
+            return expense_tuple, expense_sum, expense_prod
+    return ()
+
+def stopwatch(func, arg):
+    t_start = time.process_time_ns()
+    result = func(arg)
+    t_elapsed = time.process_time_ns() - t_start
+    return "⚙️ {} -> {} -> ⏱️ {} ms".format(func.__name__, result, t_elapsed/1000)
+
+# Main
+def main():
+    with open("expenses.txt",  "r") as f:
+        expenses = [int(expense) for expense in f.readlines()]
+        expenses = sorted(expenses)
+
+    print("Puzzle 1: find 2 expenses that sum to 2020")
+    print(stopwatch(puzzle_1_loops, expenses))
+    print(stopwatch(puzzle_1_permutations, expenses))
+
+    print("\nPuzzle 2: find 3 expenses that sum to 2020")
+    print(stopwatch(puzzle_2_loops, expenses))
+    print(stopwatch(puzzle_2_permutations, expenses))
+
+if __name__ == "__main__":
+    main()
