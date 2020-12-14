ccbeb9e581f868ad40f6a2a8d030160d49350ee4
Charles Poisson <charles.poisson@gmail.com>
2020-12-14 23:52:39 +0100

    Add day 7 refactoring

--- day_07/puzzle_solver_day07.py
+++ day_07/puzzle_solver_day07.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python3.7
 
 import time
+from collections import namedtuple
 
-def ancestors_look_up(root_node: str, vertices: list) -> list:
-    ancestors = list()
-    parents = [vertex[0] for vertex in vertices if vertex[1] == root_node]
-    for node in parents:
-        vertices = [vertex for vertex in vertices if vertex[0] not in [node, root_node]]
-        ancestors += ancestors_look_up(node, vertices)
-    return set(parents + ancestors)
+class Edge(object):
+    def __init__(self, target: str, origin: str, weight: int):
+        self.target = target
+        self.origin = origin
+        self.weight = weight
 
-def get_child_count(root_node:str, vertices: list) -> list:
-    childs = [vertex for vertex in vertices if vertex[0] == root_node]
-    child_count = 0
-    for vertex in childs:
-        child_count += (get_child_count(vertex[1], vertices) + 1) * vertex[2]
-    return child_count
+    def __hash__(self) -> int:
+        return hash('{}{}{}'.format(self.target, self.origin, self.weight))
+
+    def __repr__(self) -> str:
+        return "<{}, {}, {}>".format(self.target, self.origin, self.weight)
+
+def successors_look_up(origin: str, edges: list) -> list:
+    successors = list()
+    direct_successors = [edge.target for edge in edges if edge.origin == origin]
+    for node in direct_successors:
+        edges = [edge for edge in edges if edge.target not in [node, origin]]
+        successors += successors_look_up(node, edges)
+    return set(direct_successors + successors)
+
+def get_successors(origin:str, edges: set) -> set: # does not work, needs debug
+    direct_successors = set([edge for edge in edges if edge.origin == origin])
+    successors = set()
+    for edge in direct_successors:
+        successors |= set([edge])
+        successors |= get_successors(edge.target, edges)
+    return successors
+
+def predecessor_weighted_count(target:str, edges: list) -> list:
+    predecessors = [edge for edge in edges if edge.target == target]
+    count = 0
+    for edge in predecessors:
+        count += (predecessor_weighted_count(edge.origin, edges) + 1) * edge.weight
+    return count
 
 def part_1(input: list) -> int:
-    authorized_colors = ancestors_look_up('shiny gold', input)
-    return len(authorized_colors)
+    return len(successors_look_up('shiny gold', input))
+    #return len(get_successors('shiny gold', set(input)))
 
 def part_2(input: list) -> int:
-    return get_child_count('shiny gold', input)
+    return predecessor_weighted_count('shiny gold', input)
 
 def stopwatch(func, arg):
     t_start = time.process_time_ns()
@@ -39,13 +60,17 @@ def main():
             for __ in _[1].split(', '):
                 if __ != 'no other':
                     input.append(
-                       (_[0], '{} {}'.format(__.split(' ')[1], __.split(' ')[2]), int(__.split(' ')[0]))
+                        Edge(
+                           _[0],
+                           '{} {}'.format(__.split(' ')[1], __.split(' ')[2]),
+                           int(__.split(' ')[0])
+                        )
                     )
 
-    print("-- Part One --")
+    print("-- Part One --") # 287
     print(stopwatch(part_1, input))
 
-    print("\n-- Part Two --")
+    print("\n-- Part Two --") # 48160
     print(stopwatch(part_2, input))
 
 
